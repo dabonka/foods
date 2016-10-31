3e1345cfd60316d34729aeda564bf5cbd2e40b4a
Roman <dabonka@yandex.ru>
2016-10-31 13:57:48 +0300

    add migration

--- db/migrate/20161031085827_create_items.rb
+++ db/migrate/20161031085827_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
+      t.string  :title
+      t.string  :desctiption
+      t.integer :price
+      t.integer :type_of_course
       t.timestamps
     end
   end
--- db/migrate/20161031100221_create_line_items.rb
+++ db/migrate/20161031100221_create_line_items.rb
@@ -1,7 +1,8 @@
 class CreateLineItems < ActiveRecord::Migration[5.0]
   def change
     create_table :line_items do |t|
-
+      t.belongs_to :order, index:true
+      t.belongs_to :item, index:true
       t.timestamps
     end
   end
