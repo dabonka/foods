85217dcb87e6a0f4ac228daf0cc0717788c54e7f
Roman <dabonka@yandex.ru>
2016-11-16 01:29:58 +0300

    add correction

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "100x100>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
