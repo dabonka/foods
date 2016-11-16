2e82a517379003cca554ffebffb351a6ca2b823a
Roman <dabonka@yandex.ru>
2016-11-17 02:04:29 +0300

    add changes in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+ authorize_resource
+ 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find_by_username(params[:id])
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,15 @@
 class Order < ApplicationRecord
   belongs_to  :user
   has_and_belongs_to_many    :items
+
+  def self.sum_current_date_orders
+    sum_prices = 0
+      self.where(order_date: Date.today).find_each do |order|
+        order.items.find_each
+        sum_prices += order.items.price 
+      end
+  end
+
+
 end
 
